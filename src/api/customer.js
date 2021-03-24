c36781e29812fd06c8e7363e3b2ab1a772794dd4
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2021-03-24 13:29:39 +0800

    configuring API folder

--- src/api/customer.js
+++ src/api/customer.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const app = express.Router();
 
+// GET
+
+// POST
+
+// UPDATE
+
+// DELETE
 module.exports = app;
--- src/api/product.js
+++ src/api/product.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const app = express.Router();
 
+// GET
+
+// POST
+
+// UPDATE
+
+// DELETE
+
 module.exports = app;
